5040490db81fd5def5116e315e6bf1a136a90475
shigemk2 <i.am.shige@gmail.com>
2016-11-05 15:12:31 +0900

    Add case class

--- src/main/scala/com/example/DynamicRouter.scala
+++ src/main/scala/com/example/DynamicRouter.scala
@@ -2,5 +2,13 @@ package com.example
 
 import akka.actor._
 
+case class InterestedIn(messageType: String)
+case class NoLongerInterestedIn(messageType: String)
+
+case class TypeAMessage(description: String)
+case class TypeBMessage(description: String)
+case class TypeCMessage(description: String)
+case class TypeDMessage(description: String)
+
 object DynamicRouterDriver {
 }
